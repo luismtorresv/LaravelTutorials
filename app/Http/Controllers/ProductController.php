efe6143ce079e0325f56c19adcdf72199fe44cfe
Luis Torres <lmtorresv@eafit.edu.co>
2025-08-09 21:11:19 -0500

    Complete section C of tutorial 1

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public static $products = [
+        ['id' => '1', 'name' => 'TV', 'description' => 'Worst TV'],
+        ['id' => '2', 'name' => 'iPhone', 'description' => 'Worst iPhone'],
+        ['id' => '3', 'name' => 'Chromecast', 'description' => 'Worst Chromecast'],
+        ['id' => '4', 'name' => 'Glasses', 'description' => 'Worst Glasses'],
+    ];
+
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Products - Internet Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = ProductController::$products;
+
+        return view('product.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View
+    {
+        $viewData = [];
+        $product = ProductController::$products[$id - 1];
+        $viewData['title'] = $product['name'].' - Internet Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
+    }
+}
